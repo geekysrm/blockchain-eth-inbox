5dc6c0c078e1ec23f762db059ad4f8b393ed06b3
Soumya <soumyacool2012@gmail.com>
2018-09-01 22:29:39 +0530

    Initial commit - added compile script and started test script

---
+++ compile.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
+
+//console.log(solc.compile(source, 1));
+
+//1 is no. of contracts to be compiled
+
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
---
+++ test/Inbox.test.js
@@ -0,0 +1,3 @@
+const assert = require("assert");
+const ganache = require("ganache-cli"); //local ethereum network
+const Web3 = require("web3"); //Web3 has capital W, as we are importing a constructor func/class, i.e. the constructor is used to create an instance of the web3 library; so as a convention, we make W uppercase.
