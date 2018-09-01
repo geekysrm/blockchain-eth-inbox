4e1e89be4f134e329ed87401ee059919be84682e
Soumya <soumyacool2012@gmail.com>
2018-09-02 01:00:29 +0530

    Moved sample test to separate file

---
+++ test/sampleTest.js
@@ -0,0 +1,24 @@
+class Car {
+  park() {
+    return "stopped";
+  }
+  drive() {
+    return "vroom";
+  }
+}
+
+// any code we place inside the argument fn. of beforeEach will be executed before each it statement inside our code
+let car;
+beforeEach(() => {
+  car = new Car();
+});
+
+describe("Car", () => {
+  it("can park", () => {
+    assert.equal(car.park(), "stopped");
+  });
+
+  it("can drive", () => {
+    assert.equal(car.drive(), "vroom");
+  });
+});
