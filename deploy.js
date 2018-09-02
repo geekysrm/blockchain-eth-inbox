66668190f46cf10723dd74e9f472c863f4ba9b9e
Soumya <soumyacool2012@gmail.com>
2018-09-03 04:05:02 +0530

    Deployment done

--- deploy.js
+++ deploy.js
@@ -7,3 +7,17 @@ const provider = new HDWalletProvider(
   "https://rinkeby.infura.io/v3/c251e63904af4ff4abdd06ca2cc0ee13"
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: "0x" + bytecode,
+      arguments: ["Hi there!"]
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+  console.log("Contract deployed to", result.options.address);
+};
+
+deploy();
